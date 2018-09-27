1fc5041987046192c27504240e6aed09e7e09cf4
Selcuk Karakus <selchuck.karakus@gmail.com>
2018-09-27 14:26:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+// begining of first function
+function theBeatlesPlay(musician, instruments) {
+  let emptyArray = [];
+  for(var i = 0; i < musician.length; i++){
+    var string= musician[0] + instruments[0];
+    emptyArray.push(musician[i] + " plays " + instruments[i] );
+  }
+  return emptyArray;
+}
+// end of first function
+
+
+// begining of second function
+function johnLennonFacts(argument){
+  var array = [];
+  var i = 0;
+  while (i < argument.length){
+    array.push(argument[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+// end of second function
